5ae786d03401cc99a89db11bf55e4bf941cf66c2
Aditya Yanuar Roshadi <adityanuar@gmail.com>
2018-12-05 11:28:20 +0700

    raw where query

--- qubic.go
+++ qubic.go
@@ -1,6 +1,7 @@
 package qubic
 
 import (
+	// "fmt"
 	"reflect"
 	"strconv"
 )
@@ -102,6 +103,15 @@ func (q *Query) Where(w string, s interface{}, e bool) *Query {
 	if que != w {
 		q.Wh = append(q.Wh, que)
 	}
+	return q
+}
+
+// Raw Where query.
+// Given param as:
+// 1. raw condition in string,
+// It returns Query object itself for further query
+func (q *Query) Where_raw(w string) *Query {
+	q.Wh = append(q.Wh, w)
 	return q
 }
 
